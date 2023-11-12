e59d96a8cc67248a6ee3ecb1219cd720e2fee4c1
Tiffany Torres <torresrtiffany@gmail.com>
2023-11-11 23:54:26 -0800

    update actions.js

--- honey-bee-frontend/src/actions.js
+++ honey-bee-frontend/src/actions.js
@@ -19,3 +19,30 @@ export const createAction = async ({request}) => {
     })
     return redirect("/")
 }
+
+export const updateAction = async ({request, params}) => {
+    const formData = await request.formData()
+    const id = params.id
+    const updatedUser = {
+        subject: formData.get("subject"),
+        details: formData.get("details")
+    }
+
+    await fetch(URL + `/users/${id}/`, {
+        method: "put",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedUser)
+    })
+    return redirect(`/post/${id}`)
+}
+
+export const deleteAction = async ({params}) => {
+    const id = params.id
+
+    await fetch(URL + `/users/${id}/`, {
+        method: "delete",
+    })
+    return redirect("/")
+}
--- src/pages/Show.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Show = (props) => {
-  return <h1>Show</h1>
-}
-
-export default Show
